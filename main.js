e73954d7e1bae5ab708dcbedcf8f69ae4cfb4f1e
anniemmagh <91937613+anniemmagh@users.noreply.github.com>
2023-03-20 11:58:55 +0400

    Task 3

--- main.js
+++ main.js
@@ -12,15 +12,31 @@
 
 // 2. მომხმარებელს შემოაყვანინეთ რიცხვი და გაიგეთ არის თუ არა ეს რიცხვი ერთდროულად 2-ის და 5-ის ჯერადი
 
-const number = prompt("Enter a number:");
+// const number = prompt("Enter a number:");
 
-if (number % 2  &&  number % 5  === 0){
-    console.log(number + " " ,"რიცხვი არის 2-ის და 5-ის ჯერადი");
-}
-else if(number % 2 === 0) {
-  console.log(number + " " ,"რიცხვი არის 2-ის ჯერადი");
-} else if (number % 5 === 0) {
-  console.log(number + " " , " რიცხვი არის 5-ის ჯერადი");
-} else {
-  console.log(number + " " ," რიცხვი არ არის 2-ის და 5-ის ჯერადი");
-}
+// if (number % 2  &&  number % 5  === 0){
+//     console.log(number + " " ,"რიცხვი არის 2-ის და 5-ის ჯერადი");
+// }
+// else if(number % 2 === 0) {
+//   console.log(number + " " ,"რიცხვი არის 2-ის ჯერადი");
+// } else if (number % 5 === 0) {
+//   console.log(number + " " , " რიცხვი არის 5-ის ჯერადი");
+// } else {
+//   console.log(number + " " ," რიცხვი არ არის 2-ის და 5-ის ჯერადი");
+// }
+
+
+
+// 3. მომხმარებელს შემოაყვანინეთ ორი რიცხვი, a და b.  თუ a მეტია b-ზე a გაყავით b-ზე, თუ არა, მაშინ a გაამრავლეთ b-ზე და დააბრუნეთ პასუხი
+
+// const a = parseFloat(prompt("შეიყვანეთ რიცხვი a:"));
+// const b = parseFloat(prompt("შეიყვანეთ რიცხვი b:"));
+
+
+// if (a > b) {
+//   const result = a / b;
+//   console.log("a>b-ზე ამიტომ , a/b " + result);
+// } else {
+//   const result = a * b;
+//   console.log("a<b-ზე, ამიტომ , a*b-ზე" + result);
+// }

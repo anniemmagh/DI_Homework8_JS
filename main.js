0f8b104edacae582d52a43b2e2f91ad18b4e4403
anniemmagh <91937613+anniemmagh@users.noreply.github.com>
2023-03-20 11:54:48 +0400

    Task 2

--- main.js
+++ main.js
@@ -1,10 +1,26 @@
 
 // 1. მომხმარებელს შემოაყვანინეთ რიცხვი და გაიგეთ ეს რიცხვი ლუწია თუ კენტი
 
-const num = prompt('Enter a number: ');
-if(num %2 ==0){
-    console.log("This number is even.");
+// const num = prompt('Enter a number: ');
+// if(num %2 ==0){
+//     console.log("This number is even.");
+// }
+// else {
+//     console.log("The number is odd.");
+// }
+
+
+// 2. მომხმარებელს შემოაყვანინეთ რიცხვი და გაიგეთ არის თუ არა ეს რიცხვი ერთდროულად 2-ის და 5-ის ჯერადი
+
+const number = prompt("Enter a number:");
+
+if (number % 2  &&  number % 5  === 0){
+    console.log(number + " " ,"რიცხვი არის 2-ის და 5-ის ჯერადი");
 }
-else {
-    console.log("The number is odd.");
+else if(number % 2 === 0) {
+  console.log(number + " " ,"რიცხვი არის 2-ის ჯერადი");
+} else if (number % 5 === 0) {
+  console.log(number + " " , " რიცხვი არის 5-ის ჯერადი");
+} else {
+  console.log(number + " " ," რიცხვი არ არის 2-ის და 5-ის ჯერადი");
 }

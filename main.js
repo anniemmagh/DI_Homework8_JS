e64388639a5198aaa7847e817f1a3585dab21d93
anniemmagh <91937613+anniemmagh@users.noreply.github.com>
2023-03-20 11:42:58 +0400

    task 1

---
+++ main.js
@@ -0,0 +1,10 @@
+
+// 1. მომხმარებელს შემოაყვანინეთ რიცხვი და გაიგეთ ეს რიცხვი ლუწია თუ კენტი
+
+const num = prompt('Enter a number: ');
+if(num %2 ==0){
+    console.log("This number is even.");
+}
+else {
+    console.log("The number is odd.");
+}
